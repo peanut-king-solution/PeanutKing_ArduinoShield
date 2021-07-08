d2bfa600b9ecec3454ed27ce762590824d84799f
Jack Kwok <jackkwok@peanutkingsolution.com>
2021-07-08 20:12:49 +0800

    initial commit

---
+++ PeanutKing_ArduinoShield.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2021 PeanutKing Solution
+ *
+ * @file        PeanutKingArduinoShield.h
+ * @summary     
+ * @version     1.0
+ * @author      Jack Kwok
+ * @data        1 May 2021
+ */
+
+#include "PeanutKing_ArduinoShield.h"
+
+
+
+void I2CSend(int8_t addr, uint8_t *data, uint8_t length) {
+  Wire.beginTransmission(addr);
+  Wire.write(data, length);
+  Wire.endTransmission();
+}
+
+void I2CRead(int8_t addr, uint8_t *data, uint8_t length) {
+  uint8_t i=0;
+
+
+   Wire.requestFrom((int)addr, (int)length);
+  while (Wire.available()) {
+    data[i++] = Wire.read();
+   
+  }
+}
+
+
+color_t PeanutKingArduinoShield::readcolor(uint8_t index) {
+  
+  return rgbcolor.getcolor(index);
+}
+hsl_t PeanutKingArduinoShield::readhsl(uint8_t index) {
+  
+  return rgbcolor.gethsl(index);
+}
+rgbc_t PeanutKingArduinoShield::readrgb(uint8_t index) {
+  
+  return rgbcolor.getrgb(index);
+}
+/*                             Advance Functions
+ * ============================================================================= */
+
+// void PeanutKingArduinoShield::definePin(uint8_t index, uint8_t sensor) {
+//   multiplexerPin[index] = sensor;
+// }
+
+
+// int16_t PeanutKingArduinoShield::getSensor(uint8_t name) {
+//   byte addr, txBuff[10], rxBuff[10], length;
+
+//   txBuff[0] = addr;
+//   I2CSend(addr, txBuff, 1);
+//   I2CRead(addr, rxBuff, length);
+// }
+
+// void PeanutKingArduinoShield::setSensor(uint8_t name) {
+//   byte addr, txBuff[10], rxBuff[10], length;
+
+//   txBuff[0] = addr;
+  //I2CSend(addr, txBuff, 1);
+  //I2CRead(addr, rxBuff, 1);
+//  if (rxBuff[0]!=2) return;
+
+  // for (uint8_t i=0;i<length;i++) {
+  //   txBuff[i+1] = data[i];
+  // }
+  // I2CSend(addr, txBuff, length+1);
+// }
+
+
+/*
+void PeanutKingArduinoShield::debugconsole(const char str[]) {
+  size_t n = 0;
+  size_t l = strlen(str);
+  while (1) {
+    if (n==l)   break;
+    if (write(*str++)) n++;
+    else break;
+  }
+  return n;
+}
+
+
+uint16_t PeanutKingArduinoShield::readLaserSensor(uint8_t i) {
+  tcaselect(i);
+  return readRangeSingleMillimeters();
+}
+
+colorSensor_t PeanutKingArduinoShield::readcolorSensor(uint8_t i) {
+  static uint32_t colorTimer[5] = {0};
+  uint16_t r, g, b, c, colorTemp, lux;
+
+  tcaselect(i);
+  if ( millis() - colorTimer[i] > 24 ) {
+    getRawData(&r, &g, &b, &c);
+    colorTemp = calculateColorTemperature_dn40(r, g, b, c);
+    lux = calculateLux(r, g, b);
+    colorSensor_t s = {r, g, b, c, colorTemp, lux};
+    return s;
+  }
+}
+
+*/
+
+
+
---
+++ ledMatrix.h
@@ -0,0 +1,93 @@
+/* ledMatrix.cpp
+ * digit 7-Segment display library4
+ * Copyright (c) 2021 PeanutKing Solution
+ * Author     : Jack Kwok
+ * Create Time: 7 Jul 2021
+ * Change Log :
+ */
+
+
+
+#ifndef LEDMATRIX_H
+#define LEDMATRIX_H
+
+#include <avr/pgmspace.h>
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+//the opcodes for the MAX7221 and MAX7219
+#define OP_NOOP       0
+#define OP_DIGIT0     1
+#define OP_DIGIT1     2
+#define OP_DIGIT2     3
+#define OP_DIGIT3     4
+#define OP_DIGIT4     5
+#define OP_DIGIT5     6
+#define OP_DIGIT6     7
+#define OP_DIGIT7     8
+#define OP_DECODEMODE  9
+#define OP_INTENSITY   10
+#define OP_SCANLIMIT   11
+#define OP_SHUTDOWN    12
+#define OP_DISPLAYTEST 15
+
+class ledMatrix {
+ private:
+  byte spidata[16]; // shifte data array to the devices
+  byte status[64];  // led-status array for all 8 devices
+  int SPI_MOSI;     // Data shift outs pin
+  int SPI_CLK;      // clock signal pin
+  int SPI_CS;       // Driven LOW for chip selectzion
+  int maxDevices;   // Maximum number of devices
+
+  /* Send out a single command to the device */
+  void spiTransfer(int addr, byte opcode, byte data);
+
+ public:
+  ledMatrix(int dataPin, int clkPin, int csPin, int numDevices=1);
+
+  // Returns the number of devices on this ledMatrix
+  int getDeviceCount(void) {
+    return maxDevices;
+  }
+  // status(true: power-down mode, false: normal operation
+  void shutdown(int addr, bool status);
+
+  /* 
+   * Set the number of digits (or rows) to be displayed.
+   * See datasheet for sideeffects of the scanlimit on the brightness
+   * of the display. limit  number of digits to be displayed (1..8)
+   */
+  void setScanLimit(int addr, int limit);
+
+  // intensity: the brightness of the display. (0..15)
+  void setIntensity(int addr, int intensity);
+
+  // Switch all Leds on the display off
+  void clearDisplay(int addr);
+
+  /* Set the status of a single Led.
+   * row  the row of the Led (0..7)
+   * col  the column of the Led (0..7)
+   */
+  void setLed(int addr, int row, int col, boolean state);
+
+  /* row  row which is to be set (0..7)
+   * each bit set to 1 will light up the corresponding Led.
+   */
+  void setRow(int addr, int row, byte value);
+
+  /* col  column which is to be set (0..7)
+   * each bit set to 1 will light up the corresponding Led.
+   */
+  void setColumn(int addr, int col, byte value);
+};
+
+
+#endif  //ledMatrix.h
+
+
